092c99774e30cb9132f82ead5a23a8b015b57be6
QuantiumCodeV <quantiumcode@mail.ru>
2024-11-05 06:41:23 +0500

    fix

--- resources/views/payment/payment_add.blade.php
+++ resources/views/payment/payment_add.blade.php
@@ -70,141 +70,21 @@
 			<div class="dui"></div>
 			<div class="clear"></div>
 		</label>
-				<label class="add_sel js_radio" for="userbank_type_Сбер банк">
-			<input type="radio" name="userbank_type" value="Сбер банк" userbank_name="Сбер банк" id="userbank_type_Сбер банк" class="divhide">
-			<div class="fl mat2">Сбер банк</div>
+				<label class="add_sel js_radio" for="userbank_type_Монобанк">
+			<input type="radio" name="userbank_type" value="Монобанк" userbank_name="Монобанк" id="userbank_type_Монобанк" class="divhide">
+			<div class="fl mat2">Монобанк</div>
 			<div class="dui"></div>
 			<div class="clear"></div>
 		</label>
-				<label class="add_sel js_radio" for="userbank_type_Тінькофф банк">
-			<input type="radio" name="userbank_type" value="Тінькофф банк" userbank_name="Тінькофф банк" id="userbank_type_Тінькофф банк" class="divhide">
-			<div class="fl mat2">Тінькофф банк</div>
+				<label class="add_sel js_radio" for="userbank_type_ПриватБанк">
+			<input type="radio" name="userbank_type" value="ПриватБанк" userbank_name="ПриватБанк" id="userbank_type_ПриватБанк" class="divhide">
+			<div class="fl mat2">ПриватБанк</div>
 			<div class="dui"></div>
 			<div class="clear"></div>
 		</label>
-				<label class="add_sel js_radio" for="userbank_type_ВТБ БАНК">
-			<input type="radio" name="userbank_type" value="ВТБ БАНК" userbank_name="ВТБ БАНК" id="userbank_type_ВТБ БАНК" class="divhide">
-			<div class="fl mat2">ВТБ БАНК</div>
-			<div class="dui"></div>
-			<div class="clear"></div>
-		</label>
-				<label class="add_sel js_radio" for="userbank_type_Альфа банк">
-			<input type="radio" name="userbank_type" value="Альфа банк" userbank_name="Альфа банк" id="userbank_type_Альфа банк" class="divhide">
-			<div class="fl mat2">Альфа банк</div>
-			<div class="dui"></div>
-			<div class="clear"></div>
-		</label>
-				<label class="add_sel js_radio" for="userbank_type_OZON банк">
-			<input type="radio" name="userbank_type" value="OZON банк" userbank_name="OZON банк" id="userbank_type_OZON банк" class="divhide">
-			<div class="fl mat2">OZON банк</div>
-			<div class="dui"></div>
-			<div class="clear"></div>
-		</label>
-				<label class="add_sel js_radio" for="userbank_type_Райффайзен банк">
-			<input type="radio" name="userbank_type" value="Райффайзен банк" userbank_name="Райффайзен банк" id="userbank_type_Райффайзен банк" class="divhide">
-			<div class="fl mat2">Райффайзен банк</div>
-			<div class="dui"></div>
-			<div class="clear"></div>
-		</label>
-				<label class="add_sel js_radio" for="userbank_type_ПАТ Сбербанк">
-			<input type="radio" name="userbank_type" value="ПАТ Сбербанк" userbank_name="ПАТ Сбербанк" id="userbank_type_ПАТ Сбербанк" class="divhide">
-			<div class="fl mat2">ПАТ Сбербанк</div>
-			<div class="dui"></div>
-			<div class="clear"></div>
-		</label>
-				<label class="add_sel js_radio" for="userbank_type_Банк Відкриття">
-			<input type="radio" name="userbank_type" value="Банк Відкриття" userbank_name="Банк Відкриття" id="userbank_type_Банк Відкриття" class="divhide">
-			<div class="fl mat2">Банк Відкриття</div>
-			<div class="dui"></div>
-			<div class="clear"></div>
-		</label>
-				<label class="add_sel js_radio" for="userbank_type_Газпром банку">
-			<input type="radio" name="userbank_type" value="Газпром банку" userbank_name="Газпром банку" id="userbank_type_Газпром банку" class="divhide">
-			<div class="fl mat2">Газпром банку</div>
-			<div class="dui"></div>
-			<div class="clear"></div>
-		</label>
-				<label class="add_sel js_radio" for="userbank_type_Raiffeisen BANK">
-			<input type="radio" name="userbank_type" value="Raiffeisen BANK" userbank_name="Raiffeisen BANK" id="userbank_type_Raiffeisen BANK" class="divhide">
-			<div class="fl mat2">Raiffeisen BANK</div>
-			<div class="dui"></div>
-			<div class="clear"></div>
-		</label>
-				<label class="add_sel js_radio" for="userbank_type_Айил банк">
-			<input type="radio" name="userbank_type" value="Айил банк" userbank_name="Айил банк" id="userbank_type_Айил банк" class="divhide">
-			<div class="fl mat2">Айил банк</div>
-			<div class="dui"></div>
-			<div class="clear"></div>
-		</label>
-				<label class="add_sel js_radio" for="userbank_type_Юmoney">
-			<input type="radio" name="userbank_type" value="Юmoney" userbank_name="Юmoney" id="userbank_type_Юmoney" class="divhide">
-			<div class="fl mat2">Юmoney</div>
-			<div class="dui"></div>
-			<div class="clear"></div>
-		</label>
-				<label class="add_sel js_radio" for="userbank_type_Кредит Європа банк">
-			<input type="radio" name="userbank_type" value="Кредит Європа банк" userbank_name="Кредит Європа банк" id="userbank_type_Кредит Європа банк" class="divhide">
-			<div class="fl mat2">Кредит Європа банк</div>
-			<div class="dui"></div>
-			<div class="clear"></div>
-		</label>
-				<label class="add_sel js_radio" for="userbank_type_ТКБ Банк">
-			<input type="radio" name="userbank_type" value="ТКБ Банк" userbank_name="ТКБ Банк" id="userbank_type_ТКБ Банк" class="divhide">
-			<div class="fl mat2">ТКБ Банк</div>
-			<div class="dui"></div>
-			<div class="clear"></div>
-		</label>
-				<label class="add_sel js_radio" for="userbank_type_БРЯНСЬКЕ ВІДДІЛЕННЯ N">
-			<input type="radio" name="userbank_type" value="БРЯНСЬКЕ ВІДДІЛЕННЯ N" userbank_name="БРЯНСЬКЕ ВІДДІЛЕННЯ N" id="userbank_type_БРЯНСЬКЕ ВІДДІЛЕННЯ N" class="divhide">
-			<div class="fl mat2">БРЯНСЬКЕ ВІДДІЛЕННЯ N</div>
-			<div class="dui"></div>
-			<div class="clear"></div>
-		</label>
-				<label class="add_sel js_radio" for="userbank_type_КРАСНОДАРСЬКЕ ВІДДІЛ">
-			<input type="radio" name="userbank_type" value="КРАСНОДАРСЬКЕ ВІДДІЛ" userbank_name="КРАСНОДАРСЬКЕ ВІДДІЛ" id="userbank_type_КРАСНОДАРСЬКЕ ВІДДІЛ" class="divhide">
-			<div class="fl mat2">КРАСНОДАРСЬКЕ ВІДДІЛ</div>
-			<div class="dui"></div>
-			<div class="clear"></div>
-		</label>
-				<label class="add_sel js_radio" for="userbank_type_ВАТ Альфа-банк">
-			<input type="radio" name="userbank_type" value="ВАТ Альфа-банк" userbank_name="ВАТ Альфа-банк" id="userbank_type_ВАТ Альфа-банк" class="divhide">
-			<div class="fl mat2">ВАТ Альфа-банк</div>
-			<div class="dui"></div>
-			<div class="clear"></div>
-		</label>
-				<label class="add_sel js_radio" for="userbank_type_Отп">
-			<input type="radio" name="userbank_type" value="Отп" userbank_name="Отп" id="userbank_type_Отп" class="divhide">
-			<div class="fl mat2">Отп</div>
-			<div class="dui"></div>
-			<div class="clear"></div>
-		</label>
-				<label class="add_sel js_radio" for="userbank_type_Промсвязьбанк">
-			<input type="radio" name="userbank_type" value="Промсвязьбанк" userbank_name="Промсвязьбанк" id="userbank_type_Промсвязьбанк" class="divhide">
-			<div class="fl mat2">Промсвязьбанк</div>
-			<div class="dui"></div>
-			<div class="clear"></div>
-		</label>
-				<label class="add_sel js_radio" for="userbank_type_Ренесанс">
-			<input type="radio" name="userbank_type" value="Ренесанс" userbank_name="Ренесанс" id="userbank_type_Ренесанс" class="divhide">
-			<div class="fl mat2">Ренесанс</div>
-			<div class="dui"></div>
-			<div class="clear"></div>
-		</label>
-				<label class="add_sel js_radio" for="userbank_type_Россельхозбанк">
-			<input type="radio" name="userbank_type" value="Россельхозбанк" userbank_name="Россельхозбанк" id="userbank_type_Россельхозбанк" class="divhide">
-			<div class="fl mat2">Россельхозбанк</div>
-			<div class="dui"></div>
-			<div class="clear"></div>
-		</label>
-				<label class="add_sel js_radio" for="userbank_type_Уралсиб">
-			<input type="radio" name="userbank_type" value="Уралсиб" userbank_name="Уралсиб" id="userbank_type_Уралсиб" class="divhide">
-			<div class="fl mat2">Уралсиб</div>
-			<div class="dui"></div>
-			<div class="clear"></div>
-		</label>
-				<label class="add_sel js_radio" for="userbank_type_ПІВДЕННО-ЗАХІДНИЙ БАНК ПА">
-			<input type="radio" name="userbank_type" value="ПІВДЕННО-ЗАХІДНИЙ БАНК ПА" userbank_name="ПІВДЕННО-ЗАХІДНИЙ БАНК ПА" id="userbank_type_ПІВДЕННО-ЗАХІДНИЙ БАНК ПА" class="divhide">
-			<div class="fl mat2">ПІВДЕННО-ЗАХІДНИЙ БАНК ПА</div>
+				<label class="add_sel js_radio" for="userbank_type_Інший банк">
+			<input type="radio" name="userbank_type" value="Інший банк" userbank_name="Інший банк" id="userbank_type_Інший банк" class="divhide">
+			<div class="fl mat2">Інший банк</div>
 			<div class="dui"></div>
 			<div class="clear"></div>
 		</label>
